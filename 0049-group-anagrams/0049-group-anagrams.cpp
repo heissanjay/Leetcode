ebb8651659f00c74eb899e82b754783e65b28bc3
Sanjay J <heissanjay@gmail.com>
2023-11-12 12:35:36 +0530

    Time: 27 ms (75.97%), Space: 19.1 MB (93.03%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mapp;
+        unordered_map<long long, vector<string>> mapp;
         vector<vector<string>> result;
         
-        for (auto it : strs) {
-            string word = it;
-            sort(word.begin(), word.end());
-            mapp[word].push_back(it);
+        for (const auto& str : strs) {
+            long long hash = calculateHash(str);
+            mapp[hash].push_back(str);
         }
         
-        for (auto it: mapp) {
-            result.push_back(it.second);
+        for (const auto& entry : mapp) {
+            result.push_back(entry.second);
         }
+        
         return result;
+    }
+    
+private:
+    long long calculateHash(const string& str) {
+        const int primeNumbers[26] = {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+            41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101
+        };
+
+        const long long MOD = 1e9 + 9; 
+
+        long long hash = 1;
+        for (char ch : str) {
+            hash = (hash * primeNumbers[ch - 'a']) % MOD;
+        }
+
+        return hash;
     }
 };
