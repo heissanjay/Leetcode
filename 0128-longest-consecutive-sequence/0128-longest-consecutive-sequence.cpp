6b1636ea0a139fedd750128f1786a92210d7b45d
Sanjay J <heissanjay@gmail.com>
2023-11-14 13:51:23 +0530

    Time: 128 ms (66.60%), Space: 70.8 MB (49.39%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,32 +2,28 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
-        if (n==0) return 0;
-        
-        int res {1};
-        
-        unordered_set<int> s;
-        
-        for (const auto& num: nums) {
-            s.insert(num);
-        }
-        
-        for (const auto& it: s) {
-        
-            if (s.find(it - 1) == s.end()) {
+        if (n == 0) return 0;
+
+        unordered_set<int> s(nums.begin(), nums.end());
+
+        int res = 1;
+
+        for (const auto& num : s) {
+
+            if (s.find(num - 1) == s.end()) {
                 int count = 1;
-                int x = it;
+                int x = num + 1;
+
                 
-                
-                while (s.find(x+1) != s.end()) {
+                while (s.find(x) != s.end()) {
                     x += 1;
                     count += 1;
                 }
-                
+
                 res = max(res, count);
             }
-            
         }
+
         return res;
     }
 };
