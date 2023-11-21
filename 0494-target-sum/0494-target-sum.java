7e479be5f91728ba374271203a5c158f2ef3cc14
Sanjay J <heissanjay@gmail.com>
2023-11-21 22:48:42 +0530

    Time: 9 ms (68.21%), Space: 43.9 MB (21.99%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,28 @@
 class Solution {
- public int countExpression(int[] nums, int target, int index, int currentSum) {
-        int count = 0;
+ public int countExpression(int[] nums, int target, int index, int currentSum, int[][] memo) {
         if (index == nums.length) {
             return (currentSum == target) ? 1 : 0;
         }
         
-        count += countExpression(nums, target, index + 1, currentSum + nums[index]);
-        count += countExpression(nums, target, index + 1, currentSum - nums[index]);
-    
-        return count;
+        if (memo[index][currentSum + 1000] != -1) {
+            return memo[index][currentSum + 1000];
+        }
+        
+        int solWithPlus = countExpression(nums, target, index + 1, currentSum + nums[index], memo);
+        int solWithMinus = countExpression(nums, target, index + 1, currentSum - nums[index], memo);
+        
+        memo[index][currentSum + 1000] = solWithPlus + solWithMinus;
+        
+        return memo[index][currentSum + 1000];
     }
     
     
     public int findTargetSumWays(int[] nums, int target) {
-        return countExpression(nums, target, 0, 0);
+        int[][] memo = new int[nums.length][2001]; 
+        
+        for (int i = 0; i < memo.length; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+        return countExpression(nums, target, 0, 0, memo);
     }
 }
