cadc79afd49a147715db738e5a3a653c5ef12f2d
Sanjay J <heissanjay@gmail.com>
2023-11-25 11:13:30 +0530

    Time: 12 ms (24.23%), Space: 43.2 MB (38.40%) - LeetHub

--- 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.java
+++ 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.java
@@ -1,24 +1,16 @@
 class Solution {
     private long helper(int i, int[] nums, long currentResult, int count) {
-        if ( i >= nums.length) {
-            if ( count == 0) {
-                return Integer.MIN_VALUE;
-            }
-            return currentResult;
+        if (i >= nums.length) {
+            return count == 0 ? Integer.MIN_VALUE : currentResult;
         }
-        
-        
-        long takeIt = helper(i+1, nums, currentResult * nums[i], count +1 );
-        long skipIt = helper(i+1, nums, currentResult, count);
-        
-        currentResult = Math.max(takeIt,skipIt);
-        
-        return currentResult;
-        
-        
+
+        long takeIt = helper(i + 1, nums, currentResult * nums[i], count + 1);
+        long skipIt = helper(i + 1, nums, currentResult, count);
+
+        return Math.max(takeIt, skipIt);
     }
-    
+
     public long maxStrength(int[] nums) {
-        return helper(0, nums, 1,0);
+        return helper(0, nums, 1, 0);
     }
 }
