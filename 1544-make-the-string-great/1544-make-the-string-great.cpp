c29313ece509dd23b8a6ca8d2f752b215dd99a60
Sanjay J <heissanjay@gmail.com>
2023-11-28 20:24:59 +0530

    Time: 2 ms (58.23%), Space: 6.6 MB (93.72%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (!ans.empty() && abs(ans.back() - s[i]) == 32) {
+                ans.pop_back(); 
+            } else {
+                ans += s[i];
+            }
+        }
+        return ans;
+    }
+};
